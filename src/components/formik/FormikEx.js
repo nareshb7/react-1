b2924b33766542492bec675b1ef4eac13c398481
nareshb7 <104125516+nareshb7@users.noreply.github.com>
2022-12-09 12:30:21 +0530

    Formik Validation Added

--- src/components/formik/FormikEx.js
+++ src/components/formik/FormikEx.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import { Formik, Form, Field } from 'formik';
 
+String.prototype.ifNot = function (condition) {
+  return condition ? '' : this;
+};
+
 const FormikEx = () => {
   const handleSubmit = (value) => {
     console.log(value, 'value');
-    alert('submitted');
+    alert('Submitted, Check Console');
+  };
+  const validateFunc = ({ name, mobile, password }) => {
+    let obj = { name: '', mobile: '', password: '' };
+    if (mobile.length < 10) {
+      obj.mobile = '10 digits';
+    }
+    if (name.length < 3) {
+      obj.name = 'Min. 3 Characters';
+    }
+    const psdPattern = /^[\d]{8,}$/;
+    if (!password.match(psdPattern)) {
+      obj.password = 'Min 8 digits required';
+    }
+    return obj;
   };
   return (
     <div>
@@ -12,13 +30,32 @@ const FormikEx = () => {
       <Formik
         initialValues={{ name: '', mobile: '', password: '' }}
         onSubmit={handleSubmit}
+        validate={validateFunc}
       >
-        <Form>
-          <Field type="text" name="name" />
-          <Field type="text" name="mobile" />
-          <Field type="text" name="password" />
-          <button type="submit">Submit</button>
-        </Form>
+        {({ errors, touched }) => (
+          <Form>
+            <div>
+              <Field type="text" name="name" placeholder="Name" />
+              <div style={{ height: '25px' }}>
+                {touched.name && errors.name}
+              </div>
+            </div>
+            <div>
+              <Field type="text" name="mobile" placeholder="Mobile Number" />
+              <div style={{ height: '25px' }}>
+                {' '}
+                {touched.mobile && errors.mobile}
+              </div>
+            </div>
+            <div>
+              <Field type="text" name="password" placeholder="Password" />
+              <div style={{ height: '25px' }}>
+                {touched.password && errors.password}
+              </div>
+            </div>
+            <button type="submit">Submit</button>
+          </Form>
+        )}
       </Formik>
     </div>
   );

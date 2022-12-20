e7c8292ff0b7b5a84b6c72ff1a60d6fe31d514d0
nareshb7 <104125516+nareshb7@users.noreply.github.com>
2022-12-20 15:32:02 +0530

    1 file modified

--- src/components/ecommerce/Index.js
+++ src/components/ecommerce/Index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from './Redux/Store';
 import Navbar from './components/Navbar';
-import EcomRoutes from './EcomRoutes'
-import './EcomStyles.css'
+import EcomRoutes from './EcomRoutes';
+import './EcomStyles.css';
 
 const EcommerceMain = () => {
   return (

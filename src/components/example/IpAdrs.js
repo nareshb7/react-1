e2721a2d393b8d595eac4981d2d1ed3bc6a35349
nareshb7 <104125516+nareshb7@users.noreply.github.com>
2023-01-20 15:27:18 +0530

    Ex > IpAdrs Geo Location Added

--- src/components/example/IpAdrs.js
+++ src/components/example/IpAdrs.js
@@ -1,9 +1,27 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const IpAdrs = () => {
-  useEffect(()=> {
-    console.log('hello')
-  },[])
-  return <div>IpAdrs </div>;
+  const [data, setData] = useState({});
+  useEffect(() => {
+    console.log('hello');
+    fetch('https://geolocation-db.com/json/')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data, 'data');
+        setData(data);
+      })
+      .catch((err) => console.log(err, 'err'));
+
+    navigator.geolocation.watchPosition(function (position) {
+      console.log('Latitude is :', position.coords.latitude);
+      console.log('Longitude is :', position.coords.longitude);
+    });
+  }, []);
+  return (
+    <div>
+      <h1>IpAdrs :{data.IPv4} </h1>
+      <h1>Check the Console </h1>{' '}
+    </div>
+  );
 };
 export default IpAdrs;

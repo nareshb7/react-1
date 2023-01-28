c0e018eff6b1078e0de9419a175fa348fb27ae69
nareshb7 <104125516+nareshb7@users.noreply.github.com>
2023-01-28 07:31:34 +0530

    CSS > Loader3 nd VoiceSearch Updated

--- src/components/css/voiceSearch/VoiceSearch1.js
+++ src/components/css/voiceSearch/VoiceSearch1.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './VoiceSearch1.css';
 
 const VoiceSearch1 = () => {
+  const [isPlay, setIsPlay] = useState(true);
   return (
     <div className="mic-main">
-      <div className="mic">
-        <i className="mic-icon"></i>
-        <div className="mic-shadow"></div>
+      <h1 style={{ color: '#bcd' }}>Click the MIC Icon : </h1>
+      <h1 style={{ color: '#bca' }}>
+        Voice Search is turned {isPlay ? 'Off' : 'On'}
+      </h1>
+      <div className={isPlay ? 'mic done' : 'mic'}>
+        <i className="mic-icon" onClick={() => setIsPlay(!isPlay)}></i>
+        <div className={isPlay ? 'done mic-shadow' : 'mic-shadow'}></div>
       </div>
     </div>
   );
